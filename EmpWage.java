bcfe73c05c5628f343f86efcf938e0263e15f421
tejasdcr7 <tejasdandge@gmail.com>
2021-12-22 18:15:59 +0530

    added uc02

--- EmpWage.java
+++ EmpWage.java
@@ -4,13 +4,22 @@ import java.util.Random;
 
 public class EmpWage {
     public static void main(String[] args) {
-    int fulltime = 1;
-    double empcheck = Math.floor(Math.random() * 10) % 2;
+        int fulltime = 1;
+        int Rate=20;
+        int Emphour=0;
+        int Empwage=0;
+
+        double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == fulltime) {
-        System.out.println("Emp is Present");
-    }
-        else
+            System.out.println("Emp is Present");
+            Emphour=8;
+        }
+        else{
             System.out.println("Emp is absent");
-}
+            Emphour=0;}
+
+        Empwage = Emphour*Rate;
+        System.out.println("Empwage"+Empwage);
+    }
 
 }
